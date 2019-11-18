e2a3c5414acf84063eccb7e13e9248eed2de0af7
Rajiv Giri <rajivgiri@users.noreply.github.com>
2019-11-18 18:03:01 -0500

    First initial commit
    From Desktop to Web repo

---
+++ WebContent/angularDemo.js
@@ -0,0 +1,10 @@
+var app = angular.module("demoApp", []);
+
+app.controller("demoCtrl", function($scope){
+
+$scope.message1= "Welcome to my First AngularJS Application";
+$scope.message2= "Hello";
+$scope.message3= "How are you doing today?";
+
+});
+
